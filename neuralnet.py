4745132e07e87dd3cabda4032b6c38cdbc63ec54
Guilhem Piat <guilhempiat@yahoo.fr>
2017-03-11 18:38:23 +0100

    + Started backprop
    + added the learning step argument to constructor and attribute
    (necessary for backprop)
    + Started backprop() method :
    Done : last layer backprop.
    TODO : rest of the layers
    +/~ Added and corrected comments for readability

--- neuralnet.py
+++ neuralnet.py
@@ -11,10 +11,11 @@ class NeuralNetwork:
             - connexions
     """
 
-    def __init__(self, neuronsPerLayer):
+    def __init__(self, neuronsPerLayer, learningStep):
         """ Constructor.
             Args:
                 - neuronsPerLayer : an array containing the number of neurons for each layer
+                - the learning step, or Nu.
         """
         self.nInputs = neuronsPerLayer[0]  # number of input neurons
         self.layers = []                   # this list contains the layers. Each layer is a list of values
@@ -22,6 +23,7 @@ class NeuralNetwork:
         self.connexions = []               # We can identify the neurons involved by the connexion's index.
         # This list contains the biases for each neuron. it corresponds perfectly self.layers,
         self.biases = []                   # i.e. self.biases[i][j] is the bias for self.layers[i][j]
+        self.step = learningStep           # the learning step, or Nu.
 
         i = 0
         for layer in neuronsPerLayer:  # initializing neuron self.layers with zeroes
@@ -50,13 +52,38 @@ class NeuralNetwork:
 
         for i in range(1, len(self.layers)):  # for each layer except input layer
             for j in range(self.layers[i]):   # for each neuron
-                netj = 0                      # this is the raw value of the neuron
+                netj = 0                      # this is the raw value of the neuron (before it's passed to the sigmoid)
                 for k in range(len(self.layers[i - 1])):  # for each connexion of the current neuron to the neurons of the previous layer
-                    netj += self.layers[i - 1][k] * self.connexions[i - 1][k * len(self.layers[i]) + j]  # netj += value + connexion
+                    netj += self.layers[i - 1][k] * self.connexions[i - 1][k * len(self.layers[i]) + j]  # netj += value * connexion
                 netj += self.biases[i][j]
                 self.layers[i][j] = sigmoid(netj)
         return self.layers[len(self.layers) - 1]
 
+    def backprop(self, targetValues):
+        """ Backwards propagation algorithm.
+            This method should be called immediately after compute() : It uses the last computed value as the output to correct.
+            Args :
+                - tagetValues : a list of values corresponding to what was expected of the output neurons.
+                                Must be the same size as the number of output neurons.
+        """
+        outputLayer = self.layers[len(self.layers) - 1]
+        if len(targetValues) != len(outputLayer):
+            raise ValueError("Target list length does not match number of output neurons in network")
+
+        for k in range(targetValues):
+            # error signal dk = (Tk-Ok) f'(Netk) = (Tk-Ok) f(Netk) (1-f(Netk)) = (Tk-Ok) Ok (1-Ok)
+            dk = (targetValues[k] - outputLayer[k]) * outputLayer[k] * (1 - outputLayer[k])
+            for j in range(len(self.layers[len(self.layers) - 2])):  # for each neuron in the second-to-last layer
+                # connexion from current neuron of the second-to-last layer to current output neuron
+                WjkOld = self.connexions[len(self.layers) - 2][j * len(self.layers[len(self.layers) - 1]) + k]
+                WjkNew = WjkOld + self.learningStep * dk * self.layers[len(self.layers) - 2][j]  # Wjk_old + Nu * Dk * Oj
+                self.connexions[len(self.layers) - 2][j * len(self.layers[len(self.layers) - 1]) + k] = WjkNew
+
+        # Done : backprop on last layer
+        # TODO : backprop on the rest of the network
+
 
 def sigmoid(x):
+    """ The sigmoid function
+    """
     return 1 / (1 + math.exp(-x))
